4c8476382910ee65f860860bf30a97d2bb829fa1
Reagandennis <dennisowiti215@gmail.com>
2024-01-16 09:00:01 +0300

    CRUD functionality

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -17,6 +17,11 @@ class _HomePageState extends State<HomePage> {
   List<Map<String, dynamic>> _items = [];
 
   final _Habit_tracker = Hive.box('Habit_tracker');
+  @override
+  void initState() {
+    super.initState();
+    _refreshItems();
+  }
 
   void _refreshItems() {
     final data = _Habit_tracker.keys.map((key) {
@@ -26,15 +31,37 @@ class _HomePageState extends State<HomePage> {
 
     setState(() {
       _items = data.reversed.toList();
+      print(_items.length);
     });
   }
 
   Future<void> _createItem(Map<String, dynamic> newItem) async {
     await _Habit_tracker.add(newItem);
-    print("amount data is ${_Habit_tracker.length}");
+    _refreshItems();
+  }
+
+  Future<void> _updateItem(int itemKey, Map<String, dynamic> item) async {
+    await _Habit_tracker.put(itemKey, item);
+    _refreshItems();
+  }
+
+  Future<void> _deleteItem(int itemKey) async {
+    await _Habit_tracker.delete(itemKey);
+    _refreshItems();
+
+    // ignore: use_build_context_synchronously
+    ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('An item has been deleted')));
   }
 
   void _showFrom(BuildContext ctx, int? itemKey) async {
+    if (itemKey != null) {
+      final existingItem =
+          _items.firstWhere((element) => element['key'] == itemKey);
+      _nameController.text = existingItem['name'];
+      _quantityController.text = existingItem['quantity'].toString();
+    }
+
     showModalBottomSheet(
       context: ctx,
       elevation: 5,
@@ -62,20 +89,28 @@ class _HomePageState extends State<HomePage> {
               decoration: const InputDecoration(hintText: 'Quantity'),
             ),
             const SizedBox(
-              height: 10,
+              height: 20,
             ),
             ElevatedButton(
               onPressed: () async {
-                _createItem({
-                  "name": _nameController.text,
-                  "quantity": int.tryParse(_quantityController.text)
-                });
+                if (itemKey == null) {
+                  _createItem({
+                    "name": _nameController.text,
+                    "quantity": int.tryParse(_quantityController.text)
+                  });
+                }
+                if (itemKey != null) {
+                  _updateItem(itemKey, {
+                    'name': _nameController.text.trim(),
+                    'quantity': _quantityController.text.trim()
+                  });
+                }
                 _nameController.text = '';
                 _quantityController.text = '';
 
                 Navigator.of(context).pop();
               },
-              child: Text('Create New'),
+              child: Text(itemKey == null ? 'create new' : 'Update'),
             ),
             const SizedBox(
               height: 15,
@@ -88,25 +123,49 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "crud operatrions",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "crud opeations",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
-          ),
-          backgroundColor: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "crud opeations",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w400),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _showFrom(context, null),
-          backgroundColor: Colors.black,
-          hoverColor: Colors.grey,
-          child: const Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
+        backgroundColor: Colors.black,
+      ),
+      body: ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (_, index) {
+          final currentItem = _items[index];
+          return Card(
+            color: Colors.orange.shade100,
+            margin: const EdgeInsets.all(10),
+            elevation: 3,
+            child: ListTile(
+              title: Text(currentItem['name']),
+              subtitle: Text(currentItem['quantity'].toString()),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    onPressed: () => _showFrom(context, currentItem['key']),
+                    icon: const Icon(Icons.edit),
+                  ),
+                  IconButton(
+                    onPressed: () => _deleteItem(currentItem['key']),
+                    icon: Icon(Icons.delete),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showFrom(context, null),
+        backgroundColor: Colors.black,
+        hoverColor: Colors.grey,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
     );
