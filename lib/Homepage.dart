2cf7a2818974a04ad7a171d938ca43a011c81f4f
Reagandennis <dennisowiti215@gmail.com>
2024-01-16 08:03:23 +0300

    added the list functionality

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,51 +14,76 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _quantityController = TextEditingController();
 
+  List<Map<String, dynamic>> _items = [];
+
+  final _Habit_tracker = Hive.box('Habit_tracker');
+
+  void _refreshItems() {
+    final data = _Habit_tracker.keys.map((key) {
+      final item = _Habit_tracker.get(key);
+      return {"key": key, "name": item["name"], "quantity": item['quantity']};
+    }).toList();
+
+    setState(() {
+      _items = data.reversed.toList();
+    });
+  }
+
+  Future<void> _createItem(Map<String, dynamic> newItem) async {
+    await _Habit_tracker.add(newItem);
+    print("amount data is ${_Habit_tracker.length}");
+  }
+
   void _showFrom(BuildContext ctx, int? itemKey) async {
     showModalBottomSheet(
-        context: ctx,
-        elevation: 5,
-        isScrollControlled: true,
-        builder: (_) => Container(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(ctx).viewInsets.bottom,
-                  top: 15,
-                  left: 15,
-                  right: 15),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(hintText: 'Name'),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: _quantityController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(hintText: 'Quantity'),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      _nameController.text = '';
-                      _quantityController.text = '';
+      context: ctx,
+      elevation: 5,
+      isScrollControlled: true,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+            bottom: MediaQuery.of(ctx).viewInsets.bottom,
+            top: 15,
+            left: 15,
+            right: 15),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(hintText: 'Name'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _quantityController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(hintText: 'Quantity'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                _createItem({
+                  "name": _nameController.text,
+                  "quantity": int.tryParse(_quantityController.text)
+                });
+                _nameController.text = '';
+                _quantityController.text = '';
 
-                      Navigator.of(context).pop();
-                    },
-                    child: Text('Create New'),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  )
-                ],
-              ),
-            ));
+                Navigator.of(context).pop();
+              },
+              child: Text('Create New'),
+            ),
+            const SizedBox(
+              height: 15,
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
